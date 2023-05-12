1b4454b1249693f790644f8b1e91ed68bb70fec3
alengojkosek <alen.gojkosek@student.um.si>
2023-05-12 10:24:55 +0200

    train model and predictions to api

--- src/serve/api.py
+++ src/serve/api.py
@@ -1,12 +1,16 @@
+import os
+import pandas as pd
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/example', methods=['GET'])
-def example():
-    return jsonify({'message': 'Hello World!'})
+@app.route('/predict', methods=['GET'])
+def predict():
+    file_path = os.path.join('data', 'predictions', 'future_data.csv')
+    df = pd.read_csv(file_path)
+    return jsonify(df.to_dict(orient='records'))
 
 if __name__ == '__main__':
     app.run(debug=True)

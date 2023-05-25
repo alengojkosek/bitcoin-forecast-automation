8e8385a547f9c0bf031e8f81d408354cb79186e5
alengojkosek <alen.gojkosek@student.um.si>
2023-05-25 10:53:25 +0200

    LSTM model predictions

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,3 @@
-
 import mlflow
 import os
 import pandas as pd
@@ -13,10 +12,12 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-MLFLOW_TRACKING_URI='https://dagshub.com/alengojkosek/bitcoin-forecast-automation.mlflow'
+MLFLOW_TRACKING_URI = (
+    "https://dagshub.com/alengojkosek/bitcoin-forecast-automation.mlflow"
+)
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'alengojkosek'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = 'af9dc140062b507e1c608237487e976c3d8e7d78'
+os.environ["MLFLOW_TRACKING_USERNAME"] = "alengojkosek"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "af9dc140062b507e1c608237487e976c3d8e7d78"
 experiment = mlflow.set_experiment("btc")
 
 # Get Experiment Details
@@ -28,16 +29,15 @@ print("Lifecycle_stage: {}".format(experiment.lifecycle_stage))
 mlflow.autolog(exclusive=False)
 
 with mlflow.start_run():
+    df = pd.read_csv("data/current_data.csv")
 
-    df = pd.read_csv('data/current_data.csv')
+    df["Date"] = pd.to_datetime(df["Date"])
 
-    df['Date'] = pd.to_datetime(df['Date'])
-
-    features = ['Date', 'Close'] 
+    features = ["Date", "Close"]
     data = df[features]
 
     scaler = MinMaxScaler()
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+    scaled_data = scaler.fit_transform(data["Close"].values.reshape(-1, 1))
 
     time_window = 10
 
@@ -45,27 +45,44 @@ with mlflow.start_run():
         X = []
         y = []
         for i in range(len(data) - window_size):
-            X.append(data[i:i+window_size])
-            y.append(data[i+window_size])  
+            X.append(data[i : i + window_size])
+            y.append(data[i + window_size])
         return np.array(X), np.array(y)
 
     X, y = create_sliding_window(scaled_data, time_window)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, shuffle=False
+    )
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train, y_train, test_size=0.25, shuffle=False
+    )
 
     model = Sequential()
-    model.add(LSTM(128, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(
+        LSTM(
+            128, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])
+        )
+    )
     model.add(Dropout(0.2))
     model.add(LSTM(64))
     model.add(Dropout(0.2))
     model.add(Dense(1))
 
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss="mean_squared_error", optimizer="adam")
 
-    early_stopping = EarlyStopping(patience=3, restore_best_weights=True, monitor='loss')
+    early_stopping = EarlyStopping(
+        patience=3, restore_best_weights=True, monitor="loss"
+    )
 
-    model.fit(X_train, y_train, epochs=20, batch_size=64, callbacks=[early_stopping], verbose=1)
+    model.fit(
+        X_train,
+        y_train,
+        epochs=20,
+        batch_size=64,
+        callbacks=[early_stopping],
+        verbose=1,
+    )
 
     y_pred = model.predict(X_test)
 
@@ -80,12 +97,14 @@ with mlflow.start_run():
     print("MSE:", mse)
     print("R2 Score:", r2)
 
-    prediction_horizon = 7
-    last_date = df['Date'].iloc[0]  # Get the last date from your raw_data.csv
-    next_dates = pd.date_range(start=last_date, periods=prediction_horizon, closed='right')  # Generate the next 7 dates
+    prediction_horizon = 8
+    last_date = df["Date"].iloc[0]  # Get the last date from your raw_data.csv
+    next_dates = pd.date_range(
+        start=last_date, periods=prediction_horizon, closed="right"
+    )  # Generate the next 7 dates
 
     # Scale the last known close price
-    last_close = data['Close'].iloc[0]
+    last_close = data["Close"].iloc[0]
     scaled_last_close = scaler.transform([[last_close]])
 
     # Prepare the input sequence
@@ -95,31 +114,38 @@ with mlflow.start_run():
     # Predict the next 7 days
     predicted_prices = []
 
-    for i in range(1, prediction_horizon):
-        # Reshape the input sequence to match the model's input shape
-        input_seq = np.reshape(input_sequence[i-1], (1, input_sequence[i-1].shape[0], input_sequence[i-1].shape[1]))
+    for i in range(prediction_horizon):
+        if i > 0:
+            input_seq = np.reshape(
+                input_sequence[i - 1],
+                (1, input_sequence[i - 1].shape[0], input_sequence[i - 1].shape[1]),
+            )
 
-        # Predict the next time step
-        predicted_price = model.predict(input_seq)
+            # Predict the next time step
+            predicted_price = model.predict(input_seq)
 
-        # Append the predicted value to the input sequence
-        input_sequence[i] = np.concatenate([input_sequence[i-1, 1:], predicted_price])
+            # Append the predicted value to the input sequence
+            input_sequence[i] = np.concatenate(
+                [input_sequence[i - 1, 1:], predicted_price]
+            )
 
-        # Reverse scaling for the predicted price
-        predicted_price = scaler.inverse_transform(predicted_price)
+            # Reverse scaling for the predicted price
+            predicted_price = scaler.inverse_transform(predicted_price)
 
-        # Append the predicted price to the list of predictions
-        predicted_prices.append(predicted_price[0][0])
+            # Append the predicted price to the list of predictions
+            predicted_prices.append(predicted_price[0][0])
 
     # Print the predicted prices with dates
     for date, price in zip(next_dates, predicted_prices):
         print(f"{date.date()}: {price}")
 
     # Create a DataFrame with dates and predicted prices
-    future_data = pd.DataFrame({'Date': next_dates, 'Predicted_Price': predicted_prices})
+    future_data = pd.DataFrame(
+        {"Date": next_dates, "Predicted_Price": predicted_prices}
+    )
 
     # Save the DataFrame to CSV
-    future_data.to_csv('data/predictions/future_data.csv', index=False)
+    future_data.to_csv("data/predictions/future_data.csv", index=False)
 
     mlflow.log_metric("MAE", mae)
     mlflow.log_metric("MSE", mse)

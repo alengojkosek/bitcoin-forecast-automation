fb96a70ab159fdae3cde27935d008235c1f6f294
alengojkosek <alen.gojkosek@student.um.si>
2023-06-01 10:50:14 +0200

    Made API and metrics

--- src/serve/api.py
+++ src/serve/api.py
@@ -2,17 +2,33 @@ import os
 import pandas as pd
 from flask import Flask, jsonify
 from flask_cors import CORS
-from dagshub.streaming import install_hooks
 
 app = Flask(__name__)
 CORS(app)
 
-if __name__ == '__main__':
-    install_hooks()
-    data_path = "bitcoin-forecast-automation/data/predictions/future_data.csv"
+@app.route('/predict', methods=['GET'])
+def predict():
+    file_path = os.path.join('data', 'predictions', 'future_data.csv')
+    df = pd.read_csv(file_path)
+    return jsonify(df.to_dict(orient='records'))
+
+@app.route('/current')
+def current():
+    data_path = os.path.join(os.getcwd(), 'data', 'current_data.csv')
     if os.path.exists(data_path):
-        data = pd.read_csv(data_path)
-        print(data)
+        df = pd.read_csv(data_path)
+        return jsonify(df.to_dict(orient='records'))
     else:
-        print("Data file not found.")
+        return jsonify({'error': 'Data file not found'})
+
+@app.route('/metrics')
+def metrics():
+    data_path = os.path.join(os.getcwd(), 'data', 'metrics.csv')
+    if os.path.exists(data_path):
+        df = pd.read_csv(data_path)
+        return jsonify(df.to_dict(orient='records'))
+    else:
+        return jsonify({'error': 'Data file not found'})
+
+if __name__ == '__main__':
     app.run(debug=True)

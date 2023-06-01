2a6b97e25c3c5b56c4a8d12f3f0982c66d87ce3f
alengojkosek <alen.gojkosek@student.um.si>
2023-06-01 10:01:17 +0200

    1.6. Pipeline test

--- src/serve/api.py
+++ src/serve/api.py
@@ -2,11 +2,17 @@ import os
 import pandas as pd
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dagshub.streaming import install_hooks
 
 app = Flask(__name__)
 CORS(app)
-from dagshub.streaming import DagsHubFilesystem
-fs = DagsHubFilesystem()
 
 if __name__ == '__main__':
+    install_hooks()
+    data_path = "bitcoin-forecast-automation/data/predictions/future_data.csv"
+    if os.path.exists(data_path):
+        data = pd.read_csv(data_path)
+        print(data)
+    else:
+        print("Data file not found.")
     app.run(debug=True)
